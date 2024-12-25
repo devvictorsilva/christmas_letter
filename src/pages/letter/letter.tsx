e632bfdd07a87afa257fb730951a26d2c58f56e0
Victor <paulo9.victor.silva@gmail.com>
2024-12-25 00:21:29 -0300

    fix: show px-6 when mobile

--- src/pages/letter/letter.tsx
+++ src/pages/letter/letter.tsx
@@ -29,14 +29,14 @@ export const Letter: React.FC = () => {
 
   return (
     <div
-      className="bg-[#F3EDE1] px-6 py-8 rounded-lg flex h-[740px] flex-col gap-16"
+      className={`bg-[#F3EDE1] ${window.innerWidth <= 768 ? 'px-6' : 'px-12'} py-8 rounded-lg flex h-[740px] flex-col gap-16`}
       style={{ maxWidth: "560px", position: "relative", zIndex: 1 }}
     >
       <div>
-        <h1 className="mb-6 font-bold">{letter.letter_title}</h1>
-        <div style={{ overflowY: "auto", maxHeight: "470px", wordWrap: "break-word", overflowWrap: "break-word", whiteSpace: "pre-wrap" }}>
-          {letter.letter_content}
-        </div>
+      <h1 className="mb-6 font-bold">{letter.letter_title}</h1>
+      <div style={{ overflowY: "auto", maxHeight: "470px", wordWrap: "break-word", overflowWrap: "break-word", whiteSpace: "pre-wrap" }}>
+        {letter.letter_content}
+      </div>
       </div>
     </div>
   );
